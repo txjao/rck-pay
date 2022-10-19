02c38160b1531eb4463119acc86aba99a4b13e0b
Joao <jaobacanao@gmail.com>
2022-10-19 01:31:27 -0300

    feat: project config

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+
+
+ccBgColor01.setAttribute("fill", "green")
+ccBgColor02.setAttribute("fill", "green")
+
+const colors = {
+  visa: [],
+  mastercard: [],
+  default: []
+}
